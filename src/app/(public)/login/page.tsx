dd6bba476862399fe8bac581734c79b23e1005c1
khaitbek <khaitbek2005@gmail.com>
2023-08-16 22:48:31 +0500

    Create documentation for login page

---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+// Safarmurod aka uchun
+// browserda ko'rish uchun localhost:3000/login ga o'tiladi
+// https://ui.shadcn.com/docs/components/form -> ushbu documentationda qanday aytilgan bo'lsa xuddi shunday qilinishi kerak, hech qanday kutubxona o'rnatish shart emas hammasi o'rnatib qo'yilgan documentationdan copy paste qilib qo'yilsa bo'ldi
+
+interface LoginPageProps { }
+
+const Page: FC<LoginPageProps> = () => {
+  return (
+    <>
+      {/* kodlarni shu yerga yozib ketiladi */}
+    </>
+  )
+}
+
+export default Page;
